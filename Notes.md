8f131b9c9a55db664c4617e24b035816a6137b1c
Lisa <lisa.hl.chen@gmail.com>
2022-07-13 16:10:28 -0700

    completed files

---
+++ Notes.md
@@ -0,0 +1,9 @@
+This is an assignment for Coding Dojo.
+
+### Objectives:
+
+
+* Practice using `onmouseover` and `onmouseout`
+* Practice working from an existing code base
+
+https://www.pexels.com/videos/
---
+++ script.js
@@ -0,0 +1,19 @@
+console.log("page loaded...");
+
+function login(element){
+    element.innerText = "Logout"
+}
+
+function remove(element){
+    element.remove();
+}
+function likeNinja(){
+    alert("Ninja was liked!");
+}
+
+function hoverPlay(element){
+    element.play()
+}
+function hoverStop(element){
+    element.pause()
+}
